402e6993840acee0455398c611056bb5e7ced8f0
SomayaB <somaya.bounouar@gmail.com>
2018-01-06 18:17:58 -0500

    Add logo to header

---
+++ src/components/logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Logo = () => {
+
+  return (
+    <div>
+      <a href="/">
+        <img className="logo" src="style/reactube_logo_2017.png" />
+      </a>
+    </div>
+  );
+};
+
+export default Logo;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import Logo from './components/logo';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
@@ -36,6 +37,7 @@ class App extends Component {
     return (
       <div>
         <header className="header">
+          <Logo />
           <SearchBar onSearchTermChange={videoSearch} />
         </header>
         <VideoList
